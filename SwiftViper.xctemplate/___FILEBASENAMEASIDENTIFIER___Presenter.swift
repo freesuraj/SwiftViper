ac4235e81e0f65d20082fb79e3ec30b42826a23a
Suraj Pathak <freesuraj@gmail.com>
2016-04-25 15:57:37 +0800

    Update presenter with default init

--- SwiftViper.xctemplate/___FILEBASENAMEASIDENTIFIER___Presenter.swift
+++ SwiftViper.xctemplate/___FILEBASENAMEASIDENTIFIER___Presenter.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___Presenter: UIViewController, ___FILEBASENAMEASIDENTIFIER___PresenterProtocol, ___FILEBASENAMEASIDENTIFIER___InteractorOutputProtocol {
-    
+
     var interactor: ___FILEBASENAMEASIDENTIFIER___InteractorInputProtocol?
     // var router: RouterProtocol?
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: "___FILEBASENAMEASIDENTIFIER___Presenter", bundle: nil)
+        view.backgroundColor = UIColor.whiteColor()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,10 +29,10 @@ class ___FILEBASENAMEASIDENTIFIER___Presenter: UIViewController, ___FILEBASENAME
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
     // MARK: ___FILEBASENAMEASIDENTIFIER___InteractorOutputProtocol
     func outputToPresenter(object: AnyObject) {
-        
+
     }
 
 }
